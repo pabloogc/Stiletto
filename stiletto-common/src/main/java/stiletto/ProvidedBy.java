c8d77f16ea57b3191343d7a21fbb8967304ca53f
Pablo Orgaz <pabloogc@gmail.com>
2016-07-26 12:50:07 +0200

    Move to maven-publish from maven plugin

--- stiletto-common/src/main/java/stiletto/ProvidedBy.java
+++ stiletto-common/src/main/java/stiletto/ProvidedBy.java
@@ -7,10 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import javax.inject.Named;
-import javax.inject.Qualifier;
-
-
 public interface ProvidedBy {
 
     @Target(ElementType.METHOD)
